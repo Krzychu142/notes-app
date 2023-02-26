6969aa734237271794b502a8a561463616a8d076
Krzysztof Radzięta <kustosz142@gmail.com>
2023-02-26 17:31:59 +0100

    feat(register): complete register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [responseCode, setResponseCode] = useState(null);
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -15,42 +18,71 @@ const Register = () => {
     }));
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(formData); // tutaj można wykonać logikę rejestracji użytkownika
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/register",
+        formData
+      );
+      setResponseCode(response.status);
+      setFormData({
+        name: "",
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      setResponseCode(error.response.status);
+      setFormData({
+        name: "",
+        email: "",
+        password: "",
+      });
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-      />
-
-      <button type="submit">Register</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Register</button>
+      </form>
+      {responseCode !== null && (
+        <div>
+          {responseCode === 200 ? (
+            <p>
+              Successfully registered. Now You can{" "}
+              <Link to="/login">log in</Link>.
+            </p>
+          ) : (
+            <p>Something went wrong. Pleas try again.</p>
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
