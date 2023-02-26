edb2c1219bfc0ab76683956c502d28e3f56ff836
Krzysztof Radzięta <kustosz142@gmail.com>
2023-02-26 18:25:21 +0100

    feat(login): compleated login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/login",
+        formData
+      );
+      const token = response.data.token;
+      localStorage.setItem("token", token);
+      navigate("/notes");
+    } catch (error) {
+      setError(error);
+      setFormData({
+        email: "",
+        password: "",
+      });
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+      {error && <p>Something went wrong. Pleas try again.</p>}
+    </>
+  );
 };
 
 export default Login;
