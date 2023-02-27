47aa5a7ca9a99471247d8355737f9e4117796605
Krzysztof Radzięta <kustosz142@gmail.com>
2023-02-27 23:11:23 +0100

    feat(notes): begin on notes component, to do -> naviagte to notes after login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./components/Home";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Notes from "./components/Notes";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { isExpired } from "react-jwt";
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [isMyTokenExpired, setIsMyTokenExpired] = useState(isExpired(token));
+
+  function logOut() {
+    localStorage.removeItem("token");
+    setToken([]);
+    setIsMyTokenExpired(true);
+  }
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/" element={<Home setToken={setToken} />}></Route>
           <Route exact path="/register" element={<Register />}></Route>
           <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/notes" element={<Notes />} />
+          <Route
+            exact
+            path="/notes"
+            element={
+              <Notes
+                isMyTokenExpired={isMyTokenExpired}
+                token={token}
+                logOut={logOut}
+              />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const Notes = () => {
-  return <div>Notes</div>;
-};
+function Notes(props) {
+  const [notes, setNotes] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (props.isMyTokenExpired) {
+      navigate("/login");
+    } else {
+      axios
+        .get("http://localhost:3001/notes", {
+          headers: {
+            Authorization: `Bearer ${props.token}`,
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          setNotes(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [props.isMyTokenExpired, props.token, navigate]);
+
+  return (
+    <>
+      <header>
+        <button onClick={() => props.logOut()}>LogOut</button>
+      </header>
+      <main>
+        {notes.map((note) => (
+          <div key={note.noteId}>
+            <h2>title</h2>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </main>
+    </>
+  );
+}
 
 export default Notes;
